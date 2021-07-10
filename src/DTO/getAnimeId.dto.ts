21c9f41b0b4639b1778696bbd39f12682f7f4551
alejandro0619 <63134049+alejandro0619@users.noreply.github.com>
2021-07-10 18:14:23 -0400

    Delete src directory

--- src/DTO/getAnimeId.dto.ts
+++
@@ -1,8 +0,0 @@
-export type AnimeType = {
-  id: number,
-  type: string,
-  name: string,
-  url: string,
-  image_url: string,
-  thumbnail_url:string
-}
--- src/lib/parseScore.ts
+++
@@ -1,4 +0,0 @@
-export default (score: string): number => {
-  let tmp: string = parseFloat(score).toFixed(2);
-  return (parseFloat(tmp) - 0.01)
-}
